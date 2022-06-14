b1d5e37ffd371bb4b1ed21b58086e1b795746238
Leonam P <80845786+leonam7@users.noreply.github.com>
2022-06-14 19:15:17 -0300

    Create tabela_de_notas.php

---
+++ tabela_de_notas.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Notas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Notas cadastradas!</h1>
+    <div class="container">
+        <?php
+        echo "<table class='table'>";
+        echo "<thead>";
+        echo "<tr>";
+        echo "<th scope='col'>ID</th>";
+        echo "<th scope='col'>NOME</th>";
+        echo "<th scope='col'>Disciplina</th>";
+        echo "<th scope='col'>Nota 1</th>";
+        echo "<th scope='col'>Nota 2</th>";
+        echo "</tr>";
+        echo "</thead>";
+
+        class TableRows extends RecursiveIteratorIterator {
+            
+            function __construct($it){
+                parent ::__construct($it, self::LEAVES_ONLY);
+            }
+            function current(){
+                return "<td>". parent :: current() ."</td>";
+            }
+            function beginChildren(){
+                echo "<tr>";
+            }
+            function endChildren(){
+                echo "</tr>" . "\n";
+            }
+        }
+      
+        include "connection.php";
+        $conn = connection();
+
+        try {
+            
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT * from ratuff2rowij3xi9.notas");
+            $stmt->execute();
+       
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            foreach (new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k => $v) {
+                echo $v;
+            }
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+        echo "</table>";
+        ?>
+    </div>
+
+</body>
+</html>
