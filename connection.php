bbedced0bf0b63ae26e3627c77bd66cb97f78bf9
Leonam P <80845786+leonam7@users.noreply.github.com>
2022-06-14 19:11:59 -0300

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+function connection(){
+    $servername = "qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $username = "derasq3ycsmz8542";
+    $password = "y6xfzm8fs7nr34ro";
+    $database = "ratuff2rowij3xi9";
+try {
+$conn =  new PDO("mysql:host=$servername;database=$database;charset=utf8", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+return $conn;
+
+} catch (PDOException $e) {
+    echo "Conexão falhou! error: " . $e->getMessage();
+}
+
+}
+?>
