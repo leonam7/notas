e5ddc9957c3aba07dcc1b133d80a970340720a70
Leonam P <80845786+leonam7@users.noreply.github.com>
2022-06-14 19:12:56 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Notas</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+    <h2>Cadastre aqui sua nota!</h2>
+    <div class="container">
+        <form name="notas" id="notas" method="POST" enctype="multipart/form-data" action="cadastro.php">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Aluno</label>
+                <input type="text" class="form-control" name="nome" id="nome" aria-describedby="nomeHelp" placeholder="Nome Completo">
+            </div>
+             <div class="form-group">
+                <label for="exampleInputPassword1">Disciplina</label>
+                <input type="text" class="form-control" name="disciplina" id="disciplina" placeholder="Disciplina">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Nota 01</label>
+                <input type="number" class="form-control" name="nota1" id="nota1" placeholder="Nota">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Nota 02</label>
+                <input type="number" class="form-control" name="nota2" id="nota2" placeholder="Nota">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+    </div>  
+</body>
+</html>
